8efcfe553055a73bf2d2256b7d8ff67de56a1045
zhaogang <zhaogang@bonc.com.cn>
2018-01-19 10:12:12 +0800

    Initial commit

---
+++ src/main/java/com/beimi/web/handler/ApplicationController.java
@@ -0,0 +1,27 @@
+package com.beimi.web.handler;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.beimi.util.Menu;
+import com.beimi.web.service.repository.jpa.UserRepository;
+
+/**
+ * 应用访问转发到index页面--首页
+ */
+@Controller
+public class ApplicationController extends Handler{
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping("/")
+	@Menu(type = "apps" , subtype = "index" , access = false)
+    public ModelAndView admin(HttpServletRequest request) {
+        return request(super.createRequestPageTempletResponse("/apps/index"));
+    }
+}
